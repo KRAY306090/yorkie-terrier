06d1ae7cf1a41ab3bd049066d69dd02fd00bc277
Kennadee <kennadee.ray@gmail.com>
2020-07-16 11:10:38 -0500

    button and fetch for random breeds

--- assets/js/dogInfo.js
+++ assets/js/dogInfo.js
@@ -17,3 +17,13 @@ console.log(breedobject.name)
 
 });
 
+var getList = function() {
+    fetch(
+        `https://api.thedogapi.com/v1/breeds?api_key=74a8d6a7-fb77-4451-999a-01a85de265cc`
+        )
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+};
+
